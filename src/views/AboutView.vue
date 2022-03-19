e4100cf368e0bffe16d094a682722813ac274eaf
Lucas Gabriel <diasborges2@gmail.com>
2022-03-19 13:17:10 -0300

    Avatares

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,8 +1,37 @@
 <template>
   <div class="about">
-    <h2 class="text-h5 text-center mb-3 mt-5">Sobre o "Fake Inspect" </h2>
-    <br>
-    <p>O Fake Inspect</p>
+    <v-container>
+      <h2 class="text-h5 text-center mb-3 mt-5"> <b>Sobre o "Fake Inspect" </b> </h2>
+      <br>
+      <p>O “Fake Inspect” é um projeto desenvolvido pelos Decolers do Hackathon Decola 10ª edição Luna, que tem como objetivo ajudar na propagação de notícias verdadeiras, evitando assim as “Fake News”. O projeto foi desenvolvido por toda a equipe de Decolers e conta com diversos sites que ajudam na verificação de matérias e artigos, ajudando a combater a desinformação.
+      </p>
+      <div class="CorpoEquipe">
+
+
+        <h3> <b>Corpo da equipe: </b></h3>
+        <br>
+
+        <v-row class="pl-3" justify="right">
+          <v-avatar>
+            <img
+              src="https://cdn.vuetifyjs.com/images/john.jpg"
+              alt="John"
+            >
+            
+          </v-avatar>
+          <span class="pl-3">  Lucas Borges <br class="pr-6">
+            GitHub: @borgeslucaass
+          </span>
+          <span class="pl-6"></span>
+          
+        </v-row>
+        
+        
+        
+
+
+      </div>
+    </v-container>
   </div>
 </template>
 
@@ -12,3 +41,7 @@
     name: 'AboutView',
   }
 </script>
+
+<style scoped>
+
+</style>
