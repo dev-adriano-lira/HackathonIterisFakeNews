1e718c83cc2025f65ea3399ae713d142116480c5
Adriano <adriano.junior@ccc.ufcg.edu.br>
2022-03-19 12:34:22 -0300

    Adicionando e configurando requisições da API

--- src/views/MeuInicio.vue
+++ src/views/MeuInicio.vue
@@ -5,5 +5,19 @@
 <script>
 export default {
     name: 'MeuInicio',
-}
+
+    data() {
+      return {
+        listaSites: [],
+      };
+    },
+    created() {
+      fetch("https://it3-hbn-default-rtdb.firebaseio.com/fakeNews.json")
+      .then((resposta) => resposta.json())
+      .then((json) => {
+        this.listaSiste = json;
+        console.log(this.listaSites)
+      })
+    }
+};
 </script>
