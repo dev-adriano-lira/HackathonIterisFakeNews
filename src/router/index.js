44b56b43e2a7b919c16ad9f3707f95f00add19de
Lucas Gabriel <diasborges2@gmail.com>
2022-03-19 12:01:32 -0300

    Alteração do about

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+
 import AboutView from '../views/AboutView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
+  
   {
     path: '/about',
     name: 'about',
