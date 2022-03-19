f134debdc1c42a88b8b5933849cf0cb14da054cf
RodrigoModesto33 <rodrigomodestocq@gmail.com>
2022-03-19 12:45:46 -0300

    Correção de conflito

--- src/views/MeuInicio.vue
+++ src/views/MeuInicio.vue
@@ -2,9 +2,9 @@
   <div class="sitesList">
     <h1>Sites</h1>
     <ul>
-      <li>
-        <h3>{{ listaSites.nome }}</h3>
-        <img src="listaSites.imagem" alt="listaSites.nome" />
+      <li v-for="site of listaSites" :key="site.id">
+        <h3>{{ site.nome }}</h3>
+        <img src="site.imagem" alt="site.nome" />
         <p>
           <strong>{{ listaSites.descricao }}</strong>
         </p>
@@ -15,24 +15,19 @@
 
 <script>
 export default {
-<<<<<<< Updated upstream
-    name: 'MeuInicio',
-
-    data() {
-      return {
-        listaSites: [],
-      };
-    },
-    created() {
-      fetch("https://it3-hbn-default-rtdb.firebaseio.com/fakeNews.json")
+  name: "MeuInicio",
+  data() {
+    return {
+      listaSites: [],
+    };
+  },
+  created() {
+    fetch("https://it3-hbn-default-rtdb.firebaseio.com/fakeNews.json")
       .then((resposta) => resposta.json())
       .then((json) => {
         this.listaSiste = json;
-        console.log(this.listaSites)
-      })
-    }
-=======
-  name: "MeuInicio",
->>>>>>> Stashed changes
+        console.log(this.listaSites);
+      });
+  },
 };
 </script>
