6015d4ce4fbc1fbde8023a1fed202906e6e05518
RodrigoModesto33 <rodrigomodestocq@gmail.com>
2022-03-19 13:10:17 -0300

    Componente de lista de sites

--- src/views/MeuInicio.vue
+++ src/views/MeuInicio.vue
@@ -1,20 +1,18 @@
 <template>
-<<<<<<< HEAD
+  <<<<<<< HEAD
   <h1>Olá!</h1>
-=======
+  =======
   <div class="sitesList">
-    <h1>Sites</h1>
     <ul>
       <li v-for="site of listaSites" :key="site.id">
         <h3>{{ site.nome }}</h3>
-        <img src="site.imagem" alt="site.nome" />
+        <a :href="site.site"><img :src="site.imagem" alt="site.nome" /></a>
         <p>
-          <strong>{{ listaSites.descricao }}</strong>
+          {{ site.descricao }}
         </p>
       </li>
     </ul>
   </div>
->>>>>>> f134debdc1c42a88b8b5933849cf0cb14da054cf
 </template>
 
 <script>
@@ -29,7 +27,7 @@ export default {
     fetch("https://it3-hbn-default-rtdb.firebaseio.com/fakeNews.json")
       .then((resposta) => resposta.json())
       .then((json) => {
-        this.listaSiste = json;
+        this.listaSites = json;
         console.log(this.listaSites);
       });
   },
